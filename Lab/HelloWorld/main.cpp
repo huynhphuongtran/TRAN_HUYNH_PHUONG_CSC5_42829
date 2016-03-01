2b1d02224660ba77ca8c9f11fcc6beff3d9b6ea2
huynhphuongtran <phuongtranhuynh@gmail.com>
2016-02-29 23:32:09 -0800

    Signed-off-by: huynhphuongtran <phuongtranhuynh@gmail.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Huynh Phuong Tran
+ * Created on February 22, 2016, 3:56 AM
+ * Purpose: My first program after setup
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
+
